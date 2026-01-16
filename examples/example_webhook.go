910241679f6b0e435a4cc30dae20009b9f35ca3f
Egor Merkushev <89021645+emgeorrk@users.noreply.github.com>
2026-01-16 16:44:42 +0300

    Update subscriptions.Subscribe examples

--- examples/example_webhook.go
+++ examples/example_webhook.go
@@ -34,7 +34,7 @@ func main() {
 	for _, s := range subs.Subscriptions {
 		_, _ = api.Subscriptions.Unsubscribe(ctx, s.Url)
 	}
-	subscriptionResp, err := api.Subscriptions.Subscribe(ctx, host+"/webhook", []string{})
+	subscriptionResp, err := api.Subscriptions.Subscribe(ctx, host+"/webhook", []string{}, "my-secret-phrase")
 	log.Printf("Subscription: %#v %#v", subscriptionResp, err)
 
 	ch := make(chan schemes.UpdateInterface) // Channel with updates from Max
