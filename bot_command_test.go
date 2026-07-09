f5d2b846ee2e6026dcf72290765b1e8bd8736c70
Nikolay Chernenko <nik.chernenko@vkteam.ru>
2026-07-09 16:34:35 +0300

    midify command parse

---
+++ bot_command_test.go
@@ -0,0 +1,55 @@
+package maxbot
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/max-messenger/max-bot-api-client-go/v2/model"
+)
+
+func TestCommand(t *testing.T) {
+	cases := []struct {
+		command  string
+		expected string
+	}{
+		{
+			command:  "/help",
+			expected: "/help",
+		},
+		{
+			command:  "/help:id-773",
+			expected: "/help:id-773",
+		},
+		{
+			command:  "/help:id-773",
+			expected: "/help:id-773",
+		},
+		{
+			command:  "/help-me",
+			expected: "/help-me",
+		},
+		{
+			command:  "/help/me",
+			expected: "/help/me",
+		},
+		{
+			command:  "/help.me",
+			expected: "/help.me",
+		},
+	}
+
+	for i, c := range cases {
+		t.Run(fmt.Sprintf("case-%d", i), func(t *testing.T) {
+			m := model.Update{
+				Message: &model.MessageUpdate{
+					Body: model.MessageBody{
+						Text: c.command,
+					},
+				},
+			}
+			if c.expected != GetCommand(m) {
+				t.Errorf("expected %s, got %s", c.expected, GetCommand(m))
+			}
+		})
+	}
+}
