199885df0b4ee5309b2b370b37cf1867aee93a13
Nikolay Chernenko <nik.chernenko@vkteam.ru>
2026-08-14 18:53:21 +0300

    add callback parse

--- model/callback.go
+++ model/callback.go
@@ -1,5 +1,11 @@
 package model
 
+import (
+	"regexp"
+)
+
+var callBackReg = regexp.MustCompile(`^(?P<payload>[^:]+?)(?::(?P<param>.+))?$`)
+
 type Callback struct {
 	Timestamp  int64  `json:"timestamp"`
 	CallbackID string `json:"callback_id"`
@@ -10,4 +16,33 @@ type Callback struct {
 type CallbackAnswer struct {
 	Message      *NewMessageBody `json:"message,omitempty"`
 	Notification *string         `json:"notification,omitempty"`
+}
+
+type CallbackPayload struct {
+	Payload string
+	Param   string
+}
+
+func NewCallback(input string) (res CallbackPayload) {
+	if input == "" {
+		return
+	}
+
+	matches := callBackReg.FindStringSubmatch(input)
+	if matches == nil {
+		return
+	}
+
+	groupNames := callBackReg.SubexpNames()
+	result := make(map[string]string, len(groupNames))
+	for i, name := range groupNames {
+		if i != 0 && name != "" {
+			result[name] = matches[i]
+		}
+	}
+
+	res.Payload = result["payload"]
+	res.Param = result["param"]
+
+	return
 }
---
+++ model/callback_test.go
@@ -0,0 +1,59 @@
+package model
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestCallback(tt *testing.T) {
+	cases := []struct {
+		message string
+		payload string
+		param   string
+	}{
+		{
+			message: "command_line:param",
+			payload: "command_line",
+			param:   "param",
+		},
+		{
+			message: "command_line:333",
+			payload: "command_line",
+			param:   "333",
+		},
+		{
+			message: "command_line",
+			payload: "command_line",
+		},
+		{
+			message: "command:param1,param2",
+			payload: "command",
+			param:   "param1,param2",
+		},
+		{
+			message: "/command:1/2/3",
+			payload: "/command",
+			param:   "1/2/3",
+		},
+		{
+			message: "some command",
+			payload: "some command",
+		},
+	}
+
+	for i, tc := range cases {
+		tt.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			parsed := NewCallback(tc.message)
+
+			if parsed.Payload != tc.payload {
+				t.Errorf("payload want %q, got %q", tc.payload, parsed.Payload)
+			}
+			if parsed.Param != tc.param {
+				t.Errorf("param want %q, got %q", tc.param, parsed.Param)
+			}
+
+			// command_line
+			// command_line
+		})
+	}
+}
